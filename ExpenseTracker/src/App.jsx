d3bc657a994d7b956aabbb32f23109ef8e2931d9
subashinir05 <subashinir05@gmail.com>
2024-04-28 11:21:07 +0530

    Create pages and routes

--- ExpenseTracker/src/App.jsx
+++ ExpenseTracker/src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
